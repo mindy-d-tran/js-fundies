a8c291d0bec86bb62feab1e289264e07e876e70b
Mindy D Tran <mindytran98@gmail.com>
2023-11-28 16:36:53 -0500

    add comments to understand final result better

--- script.js
+++ script.js
@@ -125,19 +125,21 @@ function getLearnerData(course, assignGroup, submissions){
 
 /*result should look like 
 {
-    // the ID of the learner for which this data has been collected
+    // the learner's id
     "id": number,
 
     // the learner’s total, weighted average, in which assignments
     // with more points_possible should be counted for more
     // e.g. a learner with 50/100 on one assignment and 190/200 on another
     // would have a weighted average score of 240/300 = 80%.
+    // current final grade in class
     "avg": number,
 
     // each assignment should have a key with its ID,
     // and the value associated with it should be the percentage that
     // the learner scored on the assignment (submission.score / points_possible)
-    assignment_id: number
+    // grade for each assignment
+    <assignment_id>: number
 
     // if an assignment is not yet due, it should not be included in either
     // the average or the keyed dictionary of scores
